214e02722a8b88f1461b7900e54e77e4fa8530c8
Eber Alejo <eber4202@hotmail.com>
2023-11-08 19:39:28 -0600

    ADD: picom.conf, bspwm autostart

---
+++ .config/bspwm/autostart.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+#Find out your monitor name with xrandr or arandr (save and you get this line)
+#xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
+#xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
+#xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
+#xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+#autorandr horizontal
+
+#toggle layouts
+#setxkbmap -layout us,es -option grp:win_space_toggle
+setxkbmap -layout us,es -option grp:shift_caps_toggle
+
+#wallpapers
+#feh -z --bg-fill ~/Imágenes/wallpapers/**
+feh -z --bg-fill ~/Imágenes/wallpapers/minimalist/wallhaven-zxr9mj_2560x1440.png
+
+run nm-applet &
+run pamac-tray &
+run xfce4-power-manager &
+numlockx on &
+blueberry-tray &
+picom --config $HOME/.config/bspwm/picom.conf &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
+run volumeicon &
+#nitrogen --restore &
+#run caffeine &
+#run vivaldi-stable &
+#run firefox &
+#run thunar &
+#run dropbox &
+#run insync start &
+#run discord &
+#run spotify &
+#run atom &
